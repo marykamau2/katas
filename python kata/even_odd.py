41e5352bae51327a7842d92ecc656013749ac509
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-03 13:04:22 +0300

    kata

---
+++ python kata/even_odd.py
@@ -0,0 +1,7 @@
+def even_odd():
+    num = int(input('Enter number to find whether is even or odd\n'))
+    if num % 2 ==0:
+        print(f"{num} is an even number")
+    else:
+        print(f"{num} is an odd number")    
+even_odd()        
